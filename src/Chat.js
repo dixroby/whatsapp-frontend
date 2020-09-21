d2b2b97ca8c688a7c6429f9618cff15e4c20b545
dixroby arone cuellar <alestilodefe@gmail.com>
2020-09-21 01:32:49 -0500

    Feat: Diseño de chat completado.
    En esta parte se diseño el chat tanto de emisor y receptor.

--- src/Chat.js
+++ src/Chat.js
@@ -26,6 +26,25 @@ function chat() {
                     </IconButton>
                 </div>
             </div>
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Dix</span>
+                    This is message
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat__message chat__reciever">
+                    <span className="chat__name">Dix</span>
+                    This is message
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat__message">
+                    <span className="chat__name">Dix</span>
+                    This is message This is message This is message This is message v vThis is messageThis is message This is message
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+            </div>
         </div>
     )
 }
